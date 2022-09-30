acb5ec275119cda1417678fe381d325cf0e5d582
Gonzalo Fuentes <g.fuentes28@hotmail.com>
2022-09-30 17:34:01 +0200

    Initial Commit

---
+++ O2O Beer/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  O2O Beer
+//
+//  Created by Gonzalo Fuentes on 30/9/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
